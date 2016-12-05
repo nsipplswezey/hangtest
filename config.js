1fa3b90204af84dac306b8ecdb127136d5503185
Nicolas Sippl-Swezey <nicolas.sippl-swezey@gns-sd3a3d1.gateway.sonic.net>
2016-12-04 19:06:13 -0800

    Update protractor config to match https://github.com/gSchool/xp_problem_set_albums_tdd

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 exports.config = {
-  specs: ['hangtest.js']
+  specs: ['hangtest.js'],
+  mochaOpts: {
+    reporter: 'spec',
+    slow: 3000,
+    enablesTimeouts: false
+  },
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  directConnect: true
 };
